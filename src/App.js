574a6f3702c0805d1a91f88caf07a5f6c5c7bbef
adhithya <adhithyaadhithya316@gmail.com>
2023-12-07 17:49:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Name:ADHITHYA S</h1>
+        <h1>Department:B.Tech-Information Technology</h1>
+        <h1>Year:3rd Year</h1>
+        <h1>Register Number:73772121104</h1>
+        <h1>Area of Interest:Web Development</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
